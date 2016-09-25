689d6ab18b1006cb048aadc5030302c93c03bb77
marquistj13 <houpeixin@yeah.net>
2016-09-25 18:41:12 +0800

    将原程序放到basic_explore()函数中，又新建一个函数用来探索

--- type2_mf.py
+++ type2_mf.py
@@ -20,27 +20,78 @@ def exp_marginal(x, x_mu, v0, sigma_v0):
 v_expfun = np.vectorize(expfun)
 v_exp_marginal = np.vectorize(exp_marginal)
 
-fig, axs = plt.subplots(1, 3, figsize=(12, 4))
-# plot the primary membership function of x. sigma_x represents the bandwidth, we vary
-# this parameter to see the variation of membership functio
-primary_ax = axs[0]
-x = np.r_[0:25:1000j]
-x_mu = 12.5
-sigma_x = np.r_[1:4:4j]
-for i, sigma_x_i in enumerate(sigma_x):
-    primary_ax.plot(x, v_expfun(x, x_mu, sigma_x_i), '.-', color=colors[i])
-# plot the uncertainty of the estimated bandwidth
-secondary_ax = axs[1]
-v = np.r_[0:5:100j]
-v0 = 2.5  # the estimated bandwidth from data
-sigma_v0 = np.r_[0.1:1:4j]  # standard deviation (or uncertainty) of the bandwidth
-for i, sigma_v0_i in enumerate(sigma_v0):
-    secondary_ax.plot(v, v_expfun(v, v0, sigma_v0_i), '.-', color=colors[i])
-# plot the marginal fuzzy set (also called membership function), we vary
-# the hyper-parameter sigma_v0 to see its influence
-marginal_ax = axs[2]
-for i, sigma_v0_i in enumerate(sigma_v0):
-    marginal_ax.plot(x, v_exp_marginal(x, x_mu, v0, sigma_v0_i), '--', color=colors[i])
-for ax in axs:
-    ax.set_ylim([0, 1.3])
+
+def basic_explore():
+    """
+    This explore resembles Example8 in lixin wang's paper. Here  the uncertainty of the
+    bandwidth is not zero mean, i.e., it has a center:the estimated bandwidth from data
+    So I have a new formula for the marginal fuzzy set.
+    :return:
+    """
+    fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+    fig.suptitle("1 several bandwidth",fontsize=20)
+    # plot the primary membership function of x. sigma_x represents the bandwidth, we vary
+    # this parameter to see the variation of membership functio
+    primary_ax = axs[0]
+    x = np.r_[0:25:1000j]
+    x_mu = 12.5
+    sigma_x = np.r_[1:4:4j]
+    for i, sigma_x_i in enumerate(sigma_x):
+        primary_ax.plot(x, v_expfun(x, x_mu, sigma_x_i), '--', color=colors[i])
+    # plot the uncertainty of the estimated bandwidth
+    secondary_ax = axs[1]
+    v = np.r_[0:5:100j]
+    v0 = 2.5  # the estimated bandwidth from data
+    sigma_v0 = np.r_[0.1:1:4j]  # standard deviation (or uncertainty) of the bandwidth
+    for i, sigma_v0_i in enumerate(sigma_v0):
+        secondary_ax.plot(v, v_expfun(v, v0, sigma_v0_i), '.-', color=colors[i])
+    # plot the marginal fuzzy set (also called membership function), we vary
+    # the hyper-parameter sigma_v0 to see its influence
+    marginal_ax = axs[2]
+    for i, sigma_v0_i in enumerate(sigma_v0):
+        marginal_ax.plot(x, v_exp_marginal(x, x_mu, v0, sigma_v0_i), '--', color=colors[i])
+    for ax in axs:
+        ax.set_ylim([0, 1.3])
+    return None
+
+
+def specific_case():
+    """
+    This function is actually the same with the above basic_explore() except that I limit
+    the data range for my use. In fact, function basic_explore() is more general.
+    Below is the comment adapted from basic_explore():
+    This explore resembles Example8 in lixin wang's paper. Here  the uncertainty of the
+    bandwidth is not zero mean, i.e., it has a center:the estimated bandwidth from data
+    So I have a new formula for the marginal fuzzy set.
+    :return:
+    """
+    fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+    fig.suptitle("2 one bandwidth",fontsize=20)
+    # plot the primary membership function of x. sigma_x represents the bandwidth, we vary
+    # this parameter to see the variation of membership functio
+    primary_ax = axs[0]
+    x = np.r_[0:25:1000j]
+    x_mu = 12.5
+    sigma_x = np.r_[1:4:4j]
+    # for i, sigma_x_i in enumerate(sigma_x):
+    #     primary_ax.plot(x, v_expfun(x, x_mu, sigma_x_i), '.-', color=colors[i])
+    primary_ax.plot(x, v_expfun(x, x_mu, 2.5), '--', color=colors[0])
+    # plot the uncertainty of the estimated bandwidth
+    secondary_ax = axs[1]
+    v = np.r_[0:5:100j]
+    v0 = 2.5  # the estimated bandwidth from data
+    sigma_v0 = np.r_[0.1:1:4j]  # standard deviation (or uncertainty) of the bandwidth
+    for i, sigma_v0_i in enumerate(sigma_v0):
+        secondary_ax.plot(v, v_expfun(v, v0, sigma_v0_i), '.-', color=colors[i])
+    # plot the marginal fuzzy set (also called membership function), we vary
+    # the hyper-parameter sigma_v0 to see its influence
+    marginal_ax = axs[2]
+    for i, sigma_v0_i in enumerate(sigma_v0):
+        marginal_ax.plot(x, v_exp_marginal(x, x_mu, v0, sigma_v0_i), '--', color=colors[i])
+    for ax in axs:
+        ax.set_ylim([0, 1.3])
+    return None
+basic_explore()
+specific_case()
+
 plt.show()
