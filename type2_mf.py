d15d73345fab4a30afd11913a903525c6ff6599a
marquistj13 <houpeixin@yeah.net>
2016-09-25 17:07:55 +0800

    adjust format and add comments
    调整空格，增加注释

--- type2_mf.py
+++ type2_mf.py
@@ -6,45 +6,41 @@ import matplotlib.animation as animation
 colors = ['b', 'orange', 'g', 'r', 'c', 'm', 'y', 'k', 'Brown', 'ForestGreen']
 plt.style.use('ggplot')
 
-def expfun(x,mu,sigma):
-    return np.exp(-(x-mu)**2/sigma**2)
-v_expfun=np.vectorize(expfun)
-fig,axs=plt.subplots(1,3,figsize=(12,4))
 
-primary_ax=axs[0]
-x=np.r_[0:25:1000j]
-x_mu=12.5
-sigma_x=np.r_[1:4:4j]
-for i,sigma_x_i in enumerate(sigma_x):
-    primary_ax.plot(x,v_expfun(x,x_mu,sigma_x_i), '.-', color=colors[i])
+def expfun(x, mu, sigma):
+    return np.exp(-(x - mu) ** 2 / sigma ** 2)
 
-secondary_ax=axs[1]
-v=np.r_[0:5:100j]
-v0=2.5#the bandwidth
-sigma_v0=np.r_[0.1:1:4j]#standard deviation (or uncertainty) of the bandwidth
 
-for i,sigma_v0_i in enumerate(sigma_v0):
-    secondary_ax.plot(v,v_expfun(v,v0,sigma_v0_i), '.-', color=colors[i])
+def exp_marginal(x, x_mu, v0, sigma_v0):
+    d = np.abs(x - x_mu)
+    v_square = 0.5 * v0 ** 2 + sigma_v0 * d + 0.5 * v0 * np.sqrt(v0 ** 2 + 4 * sigma_v0 * d)
+    return np.exp(-d ** 2 / v_square)
 
-marginal_ax=axs[2]
-def exp_marginal(x,mu,v0,sigma_v0):
-    d=np.abs(x-mu)
-    v_square=0.5*v0**2+sigma_v0*d+0.5*v0*np.sqrt(v0**2+4*sigma_v0*d)
-    return np.exp(-d**2/v_square)
-# def exp_marginal(x,mu,v0,sigma_v0):
-#     d=np.abs(x-mu)
-#     v_square=d
-#     return np.exp(-v_square/sigma_v0**2)
 
-v_exp_marginal=np.vectorize(exp_marginal)
-# sigma_v0=np.r_[0.1:2:10]
-# sigma_v0=[1,2.5,3]
-# for i,sigma_v0_i in enumerate(sigma_v0):
-# for i,sigma_v0_i in enumerate([1,2.5,3]):
-#     marginal_ax.plot(x,v_exp_marginal(x,x_mu,v0,sigma_v0_i), '--', color=colors[i])
-for i,v0_i in enumerate([1,2.5,3]):
-    marginal_ax.plot(x,v_exp_marginal(x,x_mu,v0_i,3), '--', color=colors[i])
+v_expfun = np.vectorize(expfun)
+v_exp_marginal = np.vectorize(exp_marginal)
 
+fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+# plot the primary membership function of x. sigma_x represents the bandwidth, we vary
+# this parameter to see the variation of membership functio
+primary_ax = axs[0]
+x = np.r_[0:25:1000j]
+x_mu = 12.5
+sigma_x = np.r_[1:4:4j]
+for i, sigma_x_i in enumerate(sigma_x):
+    primary_ax.plot(x, v_expfun(x, x_mu, sigma_x_i), '.-', color=colors[i])
+# plot the uncertainty of the estimated bandwidth
+secondary_ax = axs[1]
+v = np.r_[0:5:100j]
+v0 = 2.5  # the estimated bandwidth from data
+sigma_v0 = np.r_[0.1:1:4j]  # standard deviation (or uncertainty) of the bandwidth
+for i, sigma_v0_i in enumerate(sigma_v0):
+    secondary_ax.plot(v, v_expfun(v, v0, sigma_v0_i), '.-', color=colors[i])
+# plot the marginal fuzzy set (also called membership function), we vary
+# the hyper-parameter sigma_v0 to see its influence
+marginal_ax = axs[2]
+for i, sigma_v0_i in enumerate(sigma_v0):
+    marginal_ax.plot(x, v_exp_marginal(x, x_mu, v0, sigma_v0_i), '--', color=colors[i])
 for ax in axs:
-    ax.set_ylim([0,1.3])
+    ax.set_ylim([0, 1.3])
 plt.show()
