aa983a43c2b2663ba573e94d08a520c3f7b625db
marquistj13 <houpeixin@yeah.net>
2016-09-28 21:08:48 +0800

    这个参数也可以

--- fig6_pcm_fs2.py
+++ fig6_pcm_fs2.py
@@ -32,7 +32,7 @@ def _generateFig1():
 if __name__=='__main__':
     X=_generateFig1()
     fig,ax=plt.subplots()
-    clf=pcm_fs2(X,5,0.1,alpha_cut=0.8,ax=ax,x_lim=(-1,5),y_lim=(-1,3.5))
+    clf=pcm_fs2(X,5,3,alpha_cut=0.9,ax=ax,x_lim=(-1,5),y_lim=(-1,3.5))
     # we should set "blit=False,repeat=False" or the program would fail. "init_func=clf.init_animation" plot the
     # background of each frame There is not much point to use blit=True, if most parts of your plot should be
     # refreshed. see http://stackoverflow.com/questions/14844223/python-matplotlib-blit-to-axes-or-sides-of-the
