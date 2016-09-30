71e4ff854ee6d2beba1642266fe99e419baa2d7d
marquistj13 <houpeixin@yeah.net>
2016-09-30 10:49:39 +0800

    将两个数据结合一块儿

--- plot_comprose_data_long.py
+++ plot_comprose_data_long.py
@@ -10,8 +10,22 @@ cMap = ListedColormap(['blue', 'green', 'yellow', 'red', 'black'])
 colors = ['b', 'orange', 'g', 'r', 'c', 'm', 'y', 'k', 'Brown', 'ForestGreen']
 plt.style.use('ggplot')
 
-raw_data = np.load(r'./alpha_cut_sigmaV_long.npz')
-alpha_cut, sigma_v, data = raw_data['alpha_cut'], raw_data['sigma_v'], raw_data['results']
+raw_data1 = np.load(r'./alpha_cut_sigmaV_long.npz')
+alpha_cut1, sigma_v1, data1 = raw_data1['alpha_cut'], raw_data1['sigma_v'], raw_data1['results']
+raw_data2 = np.load(r'./alpha_cut_sigmaV_long2.npz')
+alpha_cut2, sigma_v2, data2 = raw_data2['alpha_cut'], raw_data2['sigma_v'], raw_data2['results']
+
+# in raw_data1,the range of sigma_v1is [0.01,2] the step of sigma_v1 is 0.01, we need to sub-sample it.
+alpha_cut1=alpha_cut1[9::10]#result: [0.1,0.2,...,1.9]
+# we also need to sub-sample the corresponding column of data1
+data1=data1[:,9::10]
+
+#compine the two data sets
+alpha_cut=np.hstack((alpha_cut1,alpha_cut2))
+sigma_v=sigma_v1 # the two are the same
+print data1.shape,data2.shape
+data=np.hstack((data1,data2))
+
 data=data.T
 data[data >= 5] = 5
 X, Y = np.meshgrid(alpha_cut, sigma_v)
@@ -31,19 +45,12 @@ cbar.ax.set_ylabel('# of contacts', rotation=270)
 
 # put the major ticks at the middle of each cell
 ax.set_xticks(np.arange(data.shape[1]) + 0.5, minor=False)
-from  pprint import pprint
-pprint(sigma_v[9::10])
-ax.set_yticks(np.arange(data.shape[0])[9::10] + 0.5, minor=False)
+ax.set_yticks(np.arange(data.shape[0]) + 0.5, minor=False)
 # ax.invert_yaxis()
 
 #lebels
 column_labels = map(str,alpha_cut)
-row_labels = map(str,sigma_v[9::10])
+row_labels = map(str,sigma_v)
 ax.set_xticklabels(column_labels, minor=False)
 ax.set_yticklabels(row_labels, minor=False)
-
-
-# plt.grid()
-
-# plt.hexbin(X.ravel(),Y.ravel(),C=results.ravel(),gridsize=10)
 plt.show()
