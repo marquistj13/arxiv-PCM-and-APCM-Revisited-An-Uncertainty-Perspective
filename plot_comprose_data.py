0e9782ed8b9d9f0a1cd285c26c3b8414130584db
marquistj13 <houpeixin@yeah.net>
2016-09-30 09:48:52 +0800

    minor fix

--- plot_comprose_data.py
+++ plot_comprose_data.py
@@ -6,8 +6,8 @@ from matplotlib import cm
 colors = ['b', 'orange', 'g', 'r', 'c', 'm', 'y', 'k', 'Brown', 'ForestGreen']
 plt.style.use('ggplot')
 
-data = np.load(r'./alpha_cut_sigmaV.npz')
-# data = np.load(r'./alpha_cut_sigmaV_long.npz')
+# data = np.load(r'./alpha_cut_sigmaV.npz')
+data = np.load(r'./alpha_cut_sigmaV_long.npz')
 alpha_cut, sigma_v, results = data['alpha_cut'], data['sigma_v'], data['results']
 
 # # plt.hist2d(alpha_cut,sigma_v,)
@@ -19,7 +19,7 @@ alpha_cut, sigma_v, results = data['alpha_cut'], data['sigma_v'], data['results'
 #
 # # Add colorbar, make sure to specify tick locations to match desired ticklabels
 # cbar = fig.colorbar(cax)
-# # cbar.ax.set_yticklabels(['< -1', '0', '> 1'])  # vertically oriented colorbar
+# # cbar.ax.set_yticklabels(['  < -1', '0', '> 1'])  # vertically oriented colorbar
 
 X,Y=np.meshgrid(alpha_cut,sigma_v)
 plt.scatter(x=X,y=Y,c=results)
